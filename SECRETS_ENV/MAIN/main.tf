e0fb922338981edbd99f317631b306588a1c6939
Moshkin Nikita <nMoshkin@crypton.studio>
2022-04-26 17:16:24 +0700

    update

--- SECRETS_ENV/MAIN/main.tf
+++ SECRETS_ENV/MAIN/main.tf
@@ -5,18 +5,9 @@ provider "github" {
     organization = var.man_org
 }
 
-resource "null_resource" "name" {
-  provisioner "local-exec" {
-    command = "./env.sh"
-  }
-}
-
 resource "github_actions_secret" "example_secret" {
   count = length(var.name_secrets)
   repository       = "${var.repos}"
   secret_name      = "${element(var.name_secrets,count.index)}"
   plaintext_value  = "${element(var.secrets,count.index)}"
-  depends_on = [
-    null_resource.name
-  ]
 }
